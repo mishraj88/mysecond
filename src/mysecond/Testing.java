da862ccfbd03a16ef06d301c25c568457b4aa633
Jyotirmay Mishra <jyotirmaymishra@Jyotirmays-MacBook-Air.local>
2017-09-04 20:43:10 +0530

    my second project

---
+++ src/mysecond/Testing.java
@@ -0,0 +1,14 @@
+package mysecond;
+
+import org.junit.Test;
+
+public class Testing {
+public static void main(String[] args) {
+	new Testing().hello();
+}
+	@Test
+	void hello()
+	{
+		System.out.println("hello");
+	}
+}
